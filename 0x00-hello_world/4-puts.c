8759f4d853a7b2d89506a29144bb023087848587
Abdullah-Abdelgelil <abdullaha.aothman@gmail.xom>
2023-03-16 19:47:31 -0700

    a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
-int main(){
-printf("Programming is like building a multilingual puzzle\n");
-return 0;
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progtam that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
