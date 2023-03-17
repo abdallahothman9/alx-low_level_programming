53592c4a91a9e21db3c396af33c4649ca0603480
Abdullah-Abdelgelil <abdullaha.aothman@gmail.xom>
2023-03-16 19:54:08 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - Entry point
+ *
+ * Description: A C progtam taht prints with printf fucntion.
+ *
+ * Return 0 always (Succes)
+*/
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
