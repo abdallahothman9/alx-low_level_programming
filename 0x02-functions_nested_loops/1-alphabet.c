e5e5869b298f85663f817b22fec57dd9303718f4
Abdullah-Abdelgelil <abdullaha.aothman@gmail.xom>
2023-03-21 15:43:59 -0700

    trying fucnction for the first time

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *main entry point
+ *
+ *description prints _putchar using putchar prototype
+ *
+ * return always 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 9; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ *main is the entry fuchtion
+ *
+ *describtion it is printing alphabet characters
+ *
+ *always return 0
+*/
+
+int main(void)
+	{
+	printf("hello\n");
+	return (0);
+	}
