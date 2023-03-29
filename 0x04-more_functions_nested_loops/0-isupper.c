77c7df6304d292a84ba86349f39db67ce0410b8d
Abdullah-Abdelgelil <abdullaha.aothman@gmail.xom>
2023-03-28 17:46:08 -0700

    pointers first commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,17 @@
-/**
- * _isupper check if it upper case
- *
- * @c: the input alphapet
- * Return 1 if upper else return 0
-**/
-
 #include "main.h"
+
+/**
+ * _isupper - checks foruppercase letters
+ *
+ * @c: char to check
+ * Return: 0 or 1
+ */
 
 int _isupper(int c)
 {
-	if (c > 65 && c < 90)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
 		return (0);
-	
 }
 
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - Short change value of n
+ * main - assign value
+ * @n: integer to change
+ * Return: 0  always
+**/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
